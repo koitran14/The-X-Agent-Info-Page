d572c0dafd205e260f507b1ab862a9c3c2f60ccd
Trần Ngọc Đăng Khôi <khoitranngocdg@gmail.com>
2023-06-11 13:41:17 +0700

    fix scrolling to the top + into the specific section

--- src/script.js
+++ src/script.js
@@ -10,32 +10,48 @@ window.addEventListener('scroll', function() {
   });
 
   
-var menuToggle = document.getElementById('menu-icon');
-var menuIcon = document.getElementById('menu-outl');
-let navlist =  document.querySelector('.navigation');
-
-menuToggle.addEventListener('click', function() {
-  if (menuIcon.getAttribute('name') === 'menu-outline') {
-    menuIcon.setAttribute('name', 'close-outline');
-    navlist.classList.toggle('open');
-  } else {
-    menuIcon.setAttribute('name', 'menu-outline');
-    navlist.classList.remove('open');
-  }
-});
+  var menuToggle = document.getElementById('menu-bar');
+  var menuIcon = document.getElementById('menu-outl');
+  let navlist = document.querySelector('.navigation');
   
-// Select the first-page icon link
-const firstPageLink = document.querySelector('.scroll-up');
+  menuToggle.addEventListener('click', function() {
+    if (menuIcon.getAttribute('name') === 'menu-outline') {
+      menuIcon.setAttribute('name', 'close-outline');
+      navlist.classList.toggle('open');
+      menuToggle.style.top = '3%';
+      menuToggle.style.right = '20%';
+
+    } else {
+      menuIcon.setAttribute('name', 'menu-outline');
+      navlist.classList.remove('open');
+      menuToggle.style.top = ''; // Reset to the default position
+      menuToggle.style.left = ''; // Reset to the default position
+    }
+  });
+  
+// Select the scroll-up icon link
+const scrollUpLink = document.querySelector('.scroll-up');
 
 // Add a click event listener to the link
-firstPageLink.addEventListener('click', function (event) {
+scrollUpLink.addEventListener('click', function (event) {
   event.preventDefault();
 
-  // Get the target section element
-  const targetSection = document.querySelector('.intro');
-
-  // Scroll to the target section with smooth behavior
-  targetSection.scrollIntoView({
+  // Scroll to the top of the page with smooth behavior
+  window.scrollTo({
+    top: 0,
     behavior: 'smooth'
   });
 });
+
+
+
+const navLinks = document.querySelectorAll('navigation a');
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+  const currentSection = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - 1) - 1;
+
+  navLinks.forEach((link) => link.classList.remove('active'));
+  navLinks[currentSection].classList.add('active');
+});
+
